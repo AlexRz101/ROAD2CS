377522caa1c24d3eea6f69f450520ddde34f3060
Alex <Prestge.alex101@gmail.com>
2026-01-29 20:10:53 -0800

    Made RoadmapResponseDTO, basically what our backend returns

--- BE-Road2CS/src/main/java/com/road2cs/dtos/RoadmapResponseDTO.java
+++ BE-Road2CS/src/main/java/com/road2cs/dtos/RoadmapResponseDTO.java
@@ -1,4 +1,14 @@
 package com.road2cs.dtos;
 
+import com.road2cs.storage.Template;
+import com.road2cs.services.TemplatesService
+
 public class RoadmapResponseDTO {
+    private int years;
+    private String workload;
+    private boolean summer;
+    private boolean winter;
+    private int units;
+    private Template template;
+    private int recommendedUnits;
 }
