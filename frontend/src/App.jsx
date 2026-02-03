8922ffb56ccdbe27d9b3f84365ef17529168c37a
Liam Salehi <liamsalehi@protonmail.com>
2026-02-03 14:16:16 -0800

    created simple header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-  <div className="min-h-screen flex items-center justify-center bg-gray-900">
-    <h1 className="text-4xl font-bold text-white">
-      Tailwind is working 🚀
-    </h1>
-  </div>
+    <>
+    <Header/>
+    <Footer/>
+    </>
+    
   )
 }
 
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+  <div className="flex justify-between items-center">
+    <div className="">
+    <img src='/CSUN_logo.svg' className="h-16"></img>
+    </div>
+    <div> <div className="">Placeholder User's Name</div>
+    <div className="">Placeholder Student ID</div>
+    <div className="">Copmuter Science</div></div>
+   
+  </div> 
+    )
+}
