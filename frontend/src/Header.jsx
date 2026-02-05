86e1691bce8b1b94369924d13a7bd9a73a20a6b6
Liam Salehi <liamsalehi@protonmail.com>
2026-02-04 20:03:48 -0800

    added margins to header

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,13 +1,17 @@
+const name = "Placeholder User's Name"
+const studentID = "Placeholder Student ID"
+const major = "Computer Science"
+
 export default function Header() {
     return (
-  <div className="flex justify-between items-center">
-    <div className="">
-    <img src='/CSUN_logo.svg' className="h-16"></img>
-    </div>
-    <div> <div className="">Placeholder User's Name</div>
-    <div className="">Placeholder Student ID</div>
-    <div className="">Copmuter Science</div></div>
-   
-  </div> 
+        <div className="flex justify-between items-center">
+            <div className="">
+                <a href='https://www.csun.edu'> <img src='/CSUN_logo.svg' className="h-16 m-16"></img> </a>
+            </div>
+            <div className="m-16 font-serif"> <div>{name}</div>
+                <div className="">{studentID}</div>
+                <div className="">{major}</div></div>
+
+        </div>
     )
 }
