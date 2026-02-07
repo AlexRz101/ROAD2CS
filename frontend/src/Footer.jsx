d34989d65f2a962a42938e113df4491686ecd017
qrw42426 <qrw42426@gmail.com>
2026-02-07 15:47:43 -0800

    Update Footer.jsx

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -1,5 +1,17 @@
 export default function Footer() {
     return (
-        <h1></h1>
+    <footer className="fixed bottom-0 w-full bg-black text-white py-4">
+            <div className="px-8 flex justify-between items-center">
+                <div className="text-Montserrat">
+                    © {new Date().getFullYear()} California State University Northridge
+                </div>
+
+                <div className="space-x-4">
+                    <a href="https://portal.csun.edu/login" target="_blank" className="text-Montserrat underline hover:no-underline">Portal</a>
+                    <a href="https://canvas.csun.edu/" target="_blank" className="text-Montserrat underline hover:no-underline">Canvas</a>
+                    <a href="https://www.csun.edu/class-search" target="_blank" className="text-Montserrat underline hover:no-underline">Class Finder</a>
+                </div>
+            </div>
+        </footer>
     )
 }
