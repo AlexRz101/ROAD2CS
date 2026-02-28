ab8f82666e640762f9bc99505902d90cbc5ab803
Liam Salehi <liamsalehi@protonmail.com>
2026-02-27 16:22:56 -0800

    rough user input component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
+import UserChoices from './UserChoices.jsx'
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
     <Header/>
+    <UserChoices/>
     <Footer/>
     </>
     
---
+++ frontend/src/UserChoices.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+export default function UserInput() {
+    const [years, setYears] = useState('4');
+    const [workload, setWorkload] = useState('light');
+    const [winter, setWinter] = useState(false);
+    const [summer, setSummer] = useState(false);
+
+    const submit = (e) => {
+        e.preventDefault();
+        const planRequest = { years, workload, winter, summer };
+        console.log(planRequest);
+    }
+
+    return (
+
+        <div>
+            <form onSubmit={submit}>
+                <label>Years:</label>
+                <input
+                    type="number"
+                    required
+                    name="years"
+                    value={years}
+                    onChange={(e) => setYears(e.target.value)} />
+                <label>Workload: </label>
+                <select onChange={(e) => setWorkload(e.target.value)} required>
+                    <option value="light">Light</option>
+                    <option value="medium">Medium</option>
+                    <option value="heavy">Heavy</option>
+                </select>
+                <label for="winter-checkbox">Winter</label>
+                <input id="winter-checkbox" checked={winter} onChange={(e) => setWinter(prev => !prev)} type="checkbox"></input>
+                <label for="summer-checkbox">Summer</label>
+                <input id="summer-checkbox" checked={summer} onChange={(e) => setSummer(prev => !prev)} type="checkbox"></input>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
